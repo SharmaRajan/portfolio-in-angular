7b67dcb8fd80f18f594c87824d73b834c2d954e1
SharmaRajan <r7870062870@gmail.com>
2024-10-01 02:18:46 +0530

    2nd commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
+import { PageNotFoundComponent } from './error/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  // { path: 'login', component: LoginComponent},
+  { path: 'home', component: HomeComponent},
+	{ path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'not-found',component: NotFoundComponent},
+  { path: '**',component: PageNotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SharedModule } from './shared/shared.module';
+import { PageNotFoundComponent } from './error/page-not-found/page-not-found.component';
+import { InternalServerErrorComponent } from './error/internal-server-error/internal-server-error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    InternalServerErrorComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
